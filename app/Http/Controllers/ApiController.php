da52887524527ddc4a3804a252437b05a0f04884
Viacheslav Poturaev <vearutop@gmail.com>
2015-07-05 08:21:35 +0700

    no debug

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -486,7 +486,7 @@ class ApiController extends Controller
         }
 
         $this->slackResponse($_REQUEST['user_name'] . " gave $points to $userLogin for $tagName" . "\n" . $this->getMessage());
-        return 'Your opinion really matters. Thank you!' . "\n" . print_r($_REQUEST, 1);
+        return 'Your opinion really matters. Thank you!';
     }
 
 
