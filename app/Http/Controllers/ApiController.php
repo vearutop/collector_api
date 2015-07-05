4adc06b5a84056e77207d86178b79e89213675e8
Viacheslav Poturaev <vearutop@gmail.com>
2015-07-05 09:32:59 +0700

    no debug

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -218,7 +218,7 @@ class ApiController extends Controller
             return;
         }
 
-        throw new \Exception(print_r($userBadge, 1));
+        //throw new \Exception(print_r($userBadge, 1));
 
 
         $tag = Tag::where('name', self::TAG_KARMA)->first();
